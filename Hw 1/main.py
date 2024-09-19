9120c49ffe53506e45f28d1050539662df3eac64
SemenArsenyan3004 <arsenyansemen30@gmail.com>
2024-09-19 12:13:05 +0200

    Update main.py

--- Hw 1/main.py
+++ Hw 1/main.py
@@ -1,1 +1,12 @@
+def foo(a, b, c, d):
+	return((-3 + a**2) * b - 2**3) / (c - 2 *d)
+	
+a = int(input (">>>"))
+b = int(input (">>>"))
+c = int(input (">>>"))
+d = int(input (">>>"))
+print(foo(a, b, c, d))
+
+
+
 
